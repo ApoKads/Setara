ea4521edfd65100afe522a8dea85526b57f40f54
theoraas07 <theoraalpha07@gmail.com>
2025-06-15 19:02:35 +0700

    layout untuk page admin

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,14 @@
+<head>
+    <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
+
+</head>
+<body>
+    <main>
+        @yield('content')
+        @yield('scripts')
+        @yield('styles')
+    </main>
+</body>
