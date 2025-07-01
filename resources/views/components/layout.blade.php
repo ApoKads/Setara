89a476feda441e6393cc6ee2d6da89dd7055f77a
Ivan Bryliant <bryliantivan@gmail.com>
2025-07-01 13:50:46 +0700

    Penyesuaian layout.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -32,7 +32,8 @@
   </div>
 
   <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
-  @livewireScripts
+  @include('components.footer')
+  <!-- @livewireScripts -->
 </body>
 
 </html>
