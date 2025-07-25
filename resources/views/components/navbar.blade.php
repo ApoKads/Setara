3ef4dc75a6c279388981a68e78b071f5d41d8688
ApoKads <jonathantjahjana@gmail.com>
2025-07-25 10:22:06 +0700

    Pembetulan fitur pengurangan slot ketika acc / reject sebuah applicant, buat supaya yg ditmapilan ke user hanya sebuah job yg ada slotnya

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -44,9 +44,10 @@
                     {{-- Tombol Login/Sign Up (user BELUM login) --}}
                     <div class="flex items-center space-x-2 text-lg">
                         <a href="{{ route('login') }}"
-                            class="{{ request()->routeIs('login') ? 'bg-[#132442] text-white font-medium' : 'bg-white text-gray-700 hover:bg-slate-200 hover:text-black'}} px-5 py-2 transition-all duration-300 ease-in-out rounded-3xl text-center border border-[#132442]">
+                            class="{{ request()->routeIs('login') || request()->is('/') ? 'bg-[#132442] text-white font-medium' : 'bg-white text-gray-700 hover:bg-slate-200 hover:text-black' }} px-5 py-2 transition-all duration-300 ease-in-out rounded-3xl text-center border border-[#132442']">
                             Login
                         </a>
+                        
                         <a href="{{ route('signup.user.form') }}"
                             class="{{ request()->routeIs('signup.user.form') ? 'bg-[#132442] text-white font-medium' : 'bg-white text-gray-700 hover:bg-slate-200 hover:text-black'}} px-5 py-2 rounded-3xl transition-all duration-300 ease-in-out text-center border border-[#132442]">
                             Sign Up
